186fee9ccd9d49cece20f89e21bf88302e666dbc
Steve Downey <sdowney2@bloomberg.net>
2015-06-11 18:18:36 -0400

    Configure org-mode

--- prefs.el
+++ prefs.el
@@ -34,3 +34,8 @@
 (setq exordium-fci-mode :prog)
 
 (setq exordium-no-org-babel-confirm t)
+
+(setq exordium-org-export-css t)
+
+(setq exordium-org-export-css-stylesheet
+      "<link rel=\"stylesheet\" type=\"text/css\" href=\"http://sundev3.dev.bloomberg.com/~sdowney/smd-zenburn.css\" />")
