191f1496561630083f07877ce45673daa81968f8
Steve Downey <sdowney2@bloomberg.net>
2024-06-07 11:15:19 -0400

    Ninja-mode

--- after-init.el
+++ after-init.el
@@ -545,4 +545,5 @@ than the window-width are displayed with a continuation symbol."
   (let ((kill-emacs-hook) (confirm-kill-emacs nil))
     (save-buffers-kill-emacs t)))
 
+(use-package ninja-mode)
 ;;; End of file
