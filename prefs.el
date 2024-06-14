6fb85525fc9d9d5397e300fa0bd025dd483dcbad
Steve Downey <sdowney2@bloomberg.net>
2024-06-14 13:37:40 -0400

    Factor out org

--- prefs.el
+++ prefs.el
@@ -47,13 +47,6 @@
 
 (setq exordium-fci-mode :prog)
 
-(setq exordium-no-org-babel-confirm t)
-
-(setq exordium-org-export-css t)
-
-(setq exordium-org-export-css-stylesheet
-      "<link rel=\"stylesheet\" type=\"text/css\" href=\"http://sdowney.org/css/smd-zenburn.css\" />")
-
 (setq exordium-highlight-symbol t)
 
 (setq exordium-smooth-scroll nil)
